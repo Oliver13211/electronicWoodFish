3bacd22af422fda84e507d6c56feca5961edeb6c
OLIVER-PC\wangy <2995306790@qq.com>
2025-05-06 21:48:36 +0800

    1.0改

--- app/src/main/java/com/wzw/electronicWoodFish/settingActivity.java
+++ app/src/main/java/com/wzw/electronicWoodFish/settingActivity.java
@@ -1,10 +1,27 @@
 package com.wzw.electronicWoodFish;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 public class settingActivity extends AppCompatActivity {
+    public int time;
+    EditText waitTime;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        waitTime = findViewById(R.id.waitTime);
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(v -> {
+            if (waitTime.getText().length()==0 | waitTime.getText().toString().equals("0")){
+                Toast.makeText(settingActivity.this,"间隔时长不能为空或为0",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                time = Integer.parseInt(waitTime.getText().toString());
+                Toast.makeText(settingActivity.this,"间隔时长设置成功，为"+time,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
